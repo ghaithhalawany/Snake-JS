016c30148046d4263ab9119af5b3d94632c4389c
MHD Ghaith Alhelwani <ghaithh97@gmail.com>
2017-10-11 23:04:42 +0300

    The basics are done

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+var canvas = document.getElementById("cnv")
+var cnv = canvas.getContext('2d');
+
+var Height = canvas.height = 450;
+var Width = canvas.width = 450;
+
+var bgColor1 = 'rgb(220, 220, 220)';
+var bgColor2 = 'rgb(190, 190, 190)';
+
+var fps = 2;
+var timeout = 1000/fps;
+
+var snake = {
+
+}
+
+var apple = {
+
+}
+
 window.onload = function() {
 	console.log("Working Fine");
+	drawBackground();
+	setInterval(gameLoop, timeout);
+}
+
+function draw(){
+	
+}
+
+function update(){
+
+}
+
+function gameLoop(){
+	draw();
+	update();
+}
+
+function drawBackground(){
+	for(var i = 0; i < 15; i++)
+		for(var j = 0; j < 15; j++){
+			if((i + j) % 2 == 0)
+				cnv.fillStyle = bgColor1;
+			else
+				cnv.fillStyle = bgColor2;
+			cnv.fillRect(i * 30, j * 30, (i + 1) * 30, (j + 1) * 30);
+		}
 }

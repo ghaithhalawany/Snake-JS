5b669dfcfaf8607a024a074adf83668b3e35a96e
MHD Ghaith Alhelwani <ghaithh97@gmail.com>
2017-10-12 00:23:27 +0300

    snake's draw system is complete

--- script.js
+++ script.js
@@ -11,29 +11,57 @@ var fps = 2;
 var timeout = 1000/fps;
 
 var snake = {
-	
+
+	head:{
+		x: 6, // The X and Y are the position on the grid not on the canvas [0, 14]
+		y: 6
+	},
+
+	color: 'black',
+	direction: 1, // 1 is up, 2 is left, 3 is down, 4 is right
+	length: 1,
+	hash: new Array(15),
+	queue:{
+		x: [],
+		y: []
+	},
+
+	draw(){
+		for(var i = 0; i < this.queue.x.length; i++){
+			cnv.fillStyle = this.color;
+			cnv.fillRect(this.queue.x[i] * 30, this.queue.x[i] * 30, 30, 30);
+		}
+		cnv.fillStyle = 'white';
+		cnv.fillRect(this.head.x * 30 + 10, this.head.x * 30 + 10, 10, 10);
+	},
+
+	update(){
+	}
 }
 
 class Apple{
-	constructor(x, y){
+	constructor(x, y){ // The X and Y are the position on the grid not on the canvas [0, 14]
 		this.x = x;
 		this.y = y;
 		this.color = 'red';
-		this.size = 20;
+		this.size = 5;
 	} 
 	draw(){
-		
+		cnv.fillStyle = this.color;
+		cnv.beginPath();
+		cnv.arc(this.x * 30 + 15, this.y * 30 + 15, this.size, 0, 2 * Math.PI, false);
+      	cnv.fill();
 	}
 }
 
 window.onload = function() {
 	console.log("Working Fine");
-	drawBackground();
+	init();
 	setInterval(gameLoop, timeout);
 }
 
 function draw(){
-
+	snake.draw();
 }
 
 function update(){
@@ -45,7 +73,7 @@ function gameLoop(){
 	update();
 }
 
-function drawBackground(){
+function init(){
 	for(var i = 0; i < 15; i++)
 		for(var j = 0; j < 15; j++){
 			if((i + j) % 2 == 0)
@@ -54,4 +82,15 @@ function drawBackground(){
 				cnv.fillStyle = bgColor2;
 			cnv.fillRect(i * 30, j * 30, (i + 1) * 30, (j + 1) * 30);
 		}
+	// Initializing the grid
+	for(var a = 0; a < 15; a++)
+		snake.hash[a] = new Array(15);
+	for(var a = 0; a < 15; a++){
+		for(var b = 0; b < 15; b++){
+			snake.hash[a][b] = false;
+		}
+	}
+	snake.hash[snake.head.x][snake.head.x] = true;
+	snake.queue.x.push(snake.head.x);
+	snake.queue.y.push(snake.head.y);
 }

19b1474a453b7f4359cebd25b87a42f9853b5468
MHD Ghaith Alhelwani <ghaithh97@gmail.com>
2017-10-12 19:52:04 +0300

    final version

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ var cnv = canvas.getContext('2d');
 
 var Height = canvas.height = 450;
 var Width = canvas.width = 450;
+cnv.textAlign = 'center';
 
 var bgColor1 = 'rgb(220, 220, 220)';
 var bgColor2 = 'rgb(190, 190, 190)';
 
-var fps = 2;
+var fps = 5;
 var timeout = 1000/fps;
+var stopId;
 
 var apple;
+var score = 0;
 
 var snake = {
 
@@ -20,7 +23,8 @@ var snake = {
 	},
 
 	color: 'black',
-	direction: 4, // 1 is up, 2 is left, 3 is down, 4 is right
+	direction: 3, // 0 is left 1 is up, 2 is right, 3 is down
+	newDirection: 3,
 	length: 1,
 	hash: new Array(15),
 	queue:{
@@ -38,15 +42,27 @@ var snake = {
 	},
 
 	update(){
-		if(this.direction == 1)
+
+		var dif = this.direction - this.newDirection;
+		if(dif < 0) dif *= -1;
+		if(dif == 1 || dif == 3)
+			this.direction = this.newDirection;
+
+		if(this.direction == 0)
+			this.head.x--;
+		else if(this.direction == 1)
 			this.head.y--;
 		else if(this.direction == 2)
 			this.head.x++;
 		else if(this.direction == 3)
 			this.head.y++;
-		else
-			this.head.x--;
 		
+		if(checkIfLost()){
+			displayLoseInterface();
+			clearInterval(stopId);
+			return;
+		}
+
 		this.queue.x.push(this.head.x);
 		this.queue.y.push(this.head.y);
 		this.hash[this.head.x][this.head.y] = true;
@@ -55,7 +71,15 @@ var snake = {
 			this.hash[this.queue.x[0]][this.queue.y[0]] = false;
 			this.queue.x.shift();
 			this.queue.y.shift();
+		}else{
+			updateApple();
+			score++;
 		}
+
+	},
+
+	updateDirecion(newDirection){
+		this.newDirection = newDirection;
 	}
 }
 
@@ -74,12 +98,50 @@ class Apple{
 	}
 }
 
-apple = new Apple(3, 6);
+function updateApple(){
+	var x = [];
+	var y = [];
+	for(var i = 0; i < 15; i++)
+		for(var j = 0; j < 15; j++){
+			if(!snake.hash[i][j]){
+				x.push(i);
+				y.push(j);
+			}
+		}
+	var length = x.length;
+	var i = Math.floor(Math.random() * length);
+	apple = new Apple(x[i], y[i]);
+}
+
+function checkIfLost(){
+	if(snake.head.x >= 15 || snake.head.x < 0 || snake.head.y >= 15 || snake.head.y < 0){
+		clearInterval(stopId);
+		displayLoseInterface();
+		return true;
+	}
+	for(var i = 0; i < snake.queue.x.length; i++){
+		if(snake.head.x == snake.queue.x[i] && snake.head.y == snake.queue.y[i]){
+			return true;
+		}
+	}
+	return false;
+}
+
+function displayLoseInterface(){
+	cnv.font="60px Arial"; 
+	cnv.fillText("YOU LOSE", 225, 225);
+	cnv.font="30px Arial"; 
+	cnv.fillText("Score: " + score, 225, 270);
+}
 
 window.onload = function() {
 	console.log("Working Fine");
 	initSnake();
-	setInterval(gameLoop, timeout);
+	stopId = setInterval(gameLoop, timeout);
+}
+
+window.onkeydown = function(e){
+	snake.updateDirecion(e.keyCode - 37);
 }
 
 function draw(){
@@ -120,4 +182,5 @@ function initSnake(){
 	snake.hash[snake.head.x][snake.head.x] = true;
 	snake.queue.x.push(snake.head.x);
 	snake.queue.y.push(snake.head.y);
+	updateApple();
 }
